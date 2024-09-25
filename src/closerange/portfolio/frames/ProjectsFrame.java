c8252203bbf7fc80f0719e7c19209dde79c389c2
CloseRange <michael.m.hulbert@gmail.com>
2024-09-25 12:35:32 -0500

    Adding and removing projects

--- src/closerange/portfolio/frames/ProjectsFrame.java
+++ src/closerange/portfolio/frames/ProjectsFrame.java
@@ -1,16 +1,65 @@
 package closerange.portfolio.frames;
 
 import closerange.display.GuiFrame;
+import closerange.portfolio.projects.Project;
+import imgui.ImGui;
+import imgui.flag.ImGuiWindowFlags;
+import imgui.type.ImString;
 
 public class ProjectsFrame extends GuiFrame {
 
     public ProjectsFrame() {
         super("Projects");
     }
-    
+    @Override
+    public int getFlags() {
+        return ImGuiWindowFlags.MenuBar;
+    }
+
+    private static ImString name = new ImString(128);
+    private static boolean creatingProject = false;
+    private static int selected = -1;
     @Override
     public void onGui() {
+        if(ImGui.beginMenuBar()) {
+            if(ImGui.beginMenu("Edit")) {
+                if(ImGui.menuItem("Add Project")) {
+                    creatingProject = !creatingProject;
+                }
+                if(selected != -1) {
+                    if(ImGui.menuItem("Delete Selected")) {
+                        Project.removeIndex(selected);
+                        selected = -1;
+                    }
+                }
+                ImGui.endMenu();
+            }
+            ImGui.endMenuBar();
+        }
+        if(creatingProject) {
+            ImGui.text("New Project");
+            ImGui.text("Name:");
+            ImGui.inputText("##Name", name);
+            if(ImGui.button("Cancel")) {
+                creatingProject = false;
+            }
+            ImGui.sameLine();
+            if(ImGui.button("Create")) {
+                if(name.get().length() == 0) return;
+                Project.create(name.get());
+                name.clear();
+                creatingProject = false;
+            }
+            ImGui.separator();
+        }
 
+        int index = 0;
+        for(Project proj : Project.getAll()) {
+            if(ImGui.selectable(proj.name, selected == index)) {
+                selected = (selected == index) ? -1 : index;
+            }
+            index++;
+        }
     }
     
 }
---
+++ src/closerange/portfolio/projects/Project.java
@@ -0,0 +1,70 @@
+package closerange.portfolio.projects;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import closerange.portfolio.debug.Debug;
+import closerange.portfolio.util.Loader;
+
+public class Project {
+    public static final String PROJECTS_LOCATION = "/projects.json";
+    
+    public String name;
+    public String description;
+    public String[] images;
+    public String[] links;
+
+    
+
+
+    private static ArrayList<Project> projects = null;
+    public static ArrayList<Project> getAll() {
+        if(projects == null) {
+            Gson gson = new Gson();
+            try {
+                String path = Loader.getProjectPath() + PROJECTS_LOCATION;
+                if(!(new File(path).exists())) {
+                    projects = new ArrayList<>();
+                    return projects;
+                }
+                String inFile = new String(Files.readAllBytes(Paths.get(path)));
+                projects = gson.fromJson(inFile, new TypeToken<ArrayList<Project>>(){}.getType());
+            } catch(Exception e) {
+                Debug.error("Error loading linker file: " + e.getMessage());
+            }
+        }
+        return projects;
+    }
+    public static void removeIndex(int i) {
+        getAll();
+        if(i < projects.size() && i >= 0) {
+            projects.remove(i);
+            save();
+        }
+
+    }
+    public static void create(String name) {
+        Project proj = new Project();
+        proj.name = name;
+        projects.add(proj);
+        save();
+    }
+    private static void save() {
+        // save
+        Gson gson = new Gson();
+        String json = gson.toJson(projects);
+        try {
+            String path = Loader.getProjectPath() + PROJECTS_LOCATION;
+            Files.write(Paths.get(path), json.getBytes());
+        } catch(Exception e) {
+            Debug.error("Error saving linker file: " + e.getMessage());
+        }
+    }
+}
+
+
